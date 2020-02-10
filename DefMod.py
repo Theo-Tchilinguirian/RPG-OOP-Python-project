3ae921c7bd38ad57f68a7b91aa0e4e891ca055ff
Theo-Tchilinguirian <60852187+Theo-Tchilinguirian@users.noreply.github.com>
2020-02-10 15:01:13 +0100

    Version 0.0125 x] alpha on va dire

--- DefMod.py
+++ DefMod.py
@@ -17,57 +17,101 @@ import random
 
 class Player:
 
-    def __init__(self, cls, lvl, mon, hp, atk, dfs, chp, klc):
-        self.player_stats = {'CLASS': cls, 'LEVEL': lvl, 'MONEY': mon, 'HP': hp, 'ATTACK': atk, 'DEFENSE': dfs, 'CHAPTER': chp, 'KILLCOUNT': klc}
+    def __init__(self, cls, lvl, mon, hp, atk, dfs, chp, klc, bhp, kcm):
+
+        self.player_stats = {'CLASS': cls, 'LEVEL': lvl, 'MONEY': mon, 'HP': hp, 'ATTACK': atk, 'DEFENSE': dfs,
+                              'CHAPTER': chp, 'KILLCOUNT': klc, 'BASEHP': bhp, 'KILLCOUNTMAX': kcm}
 
     def set_stat_value(self, stat_key, stat_new_value):
+
         self.player_stats[stat_key] = stat_new_value
 
+
     def get_player_stats(self):
+
         return self.player_stats
 
+
+    def act_player_attacking(self):
+
+        pass
+        if 'VIGOR' in self.player_stats:
+            pass
+        elif 'MANA' in self.player_stats:
+            pass
+
+
+    def player_level_up(self, player_list, pseudonyme):
+
+        kill_count_max = self.player_stats['KILLCOUNTMAX']
+        current_player_kill_count = self.get_player_stats()['KILLCOUNT']
+
+        if current_player_kill_count % kill_count_max == 0:
+
+            for stat_key, current_stat_value in self.player_stats.items():
+
+                if stat_key == 'LEVEL' or stat_key == 'KILLCOUNTMAX':
+                    self.set_stat_value(stat_key, current_stat_value + 1)
+
+                elif stat_key == 'BASEHP':
+                    new_stat_value = round(current_stat_value + (current_stat_value * 15) / 100)
+                    self.set_stat_value(stat_key, new_stat_value)
+                    self.set_stat_value('HP', new_stat_value)  # The player gets full hp every level
+
+                elif stat_key == 'ATTACK' or stat_key == 'DEFENSE':
+                    new_stat_value = round(current_stat_value + (current_stat_value / 10))
+                    self.set_stat_value(stat_key, new_stat_value)
+
+                print(stat_key, current_stat_value)
+        player_entity = self
+        player_list[pseudonyme][1] = player_entity
+
+        return player_list, player_entity
+
+
+# mana? vigueur?
 
 class Warrior(Player):
 
     def __init__(self, vig = 80):
-        Player.__init__(self, cls = 'Warrior', lvl = 0, mon = 0, hp = 100, atk = 35, dfs = 35, chp = 'I: THE RUINS', klc = 0)
-        self.player_stats["VIGOR"] = vig
+        Player.__init__(self, cls = 'Warrior', lvl = 1, mon = 0, hp = 100, atk = 35, dfs = 35, chp = 'I: THE RUINS', klc = 0, bhp = 100, kcm = 5)
+        self.set_stat_value('VIGOR', vig)
 
 
 class Soldier(Player):
 
     def __init__(self, vig = 90):
-        Player.__init__(self, cls='Soldier', lvl=0, mon=0, hp=90, atk=40, dfs=60, chp = 'I: THE RUINS', klc = 0)
-        self.player_stats["VIGOR"] = vig
+        Player.__init__(self, cls='Soldier', lvl = 1, mon = 0, hp = 90, atk = 40, dfs = 60, chp = 'I: THE RUINS', klc = 0, bhp = 90, kcm = 5)
+        self.set_stat_value('VIGOR', vig)
 
 
 class Gladiator(Player):
 
     def __init__(self, vig = 100):
-        Player.__init__(self, cls='Gladiator', lvl=0, mon=0, hp=80, atk=35, dfs=35, chp = 'I: THE RUINS', klc = 0)
-        self.player_stats["VIGOR"] = vig
+        Player.__init__(self, cls='Gladiator', lvl = 1, mon = 0, hp = 80, atk = 35, dfs = 35, chp = 'I: THE RUINS', klc = 0, bhp = 80, kcm = 5)
+        self.set_stat_value('VIGOR', vig)
 
 
 class Mage(Player):
 
     def __init__(self, mana = 90):
-        Player.__init__(self, cls='Mage', lvl=0, mon=0, hp=100, atk=25, dfs=35, chp = 'I: THE RUINS', klc = 0)
-        self.player_stats["MANA"] = mana
+        Player.__init__(self, cls='Mage', lvl = 1, mon = 0, hp = 100, atk = 25, dfs = 35, chp = 'I: THE RUINS', klc = 0, bhp = 100, kcm = 5)
+        self.set_stat_value('MANA', mana)
 
 
 class Sorcerer(Player):
 
     def __init__(self, mana = 80):
-        Player.__init__(self, cls='Sorcerer', lvl=0, mon=0, hp=110, atk=30, dfs=30, chp = 'I: THE RUINS', klc = 0)
-        self.player_stats["MANA"] = mana
+        Player.__init__(self, cls='Sorcerer', lvl = 1, mon = 0, hp = 110, atk = 30, dfs = 30, chp = 'I: THE RUINS', klc = 0, bhp = 110, kcm = 5)
+        self.set_stat_value('MANA', mana)
 
 
 
 class Priest(Player):
 
     def __init__(self, mana = 110):
-        Player.__init__(self, cls='Priest', lvl=0, mon=0, hp=75, atk=20, dfs=25, chp = 'I: THE RUINS', klc = 0)
-        self.player_stats["MANA"] = mana
+        Player.__init__(self, cls='Priest', lvl = 1, mon = 0, hp = 75, atk = 20, dfs = 25, chp = 'I: THE RUINS', klc = 0, bhp = 75, kcm = 5)
+        self.set_stat_value('MANA', mana)
 
 
 
@@ -119,8 +163,8 @@ def show_player_list(player_list):
 
 
 def check_answer_get_class_stats():
-    condition = 1
-    while condition == 1:
+    end = False
+    while end == False:
         try:
             answer_class = int(input("""What class do you want to be ?
             EASY:
@@ -135,7 +179,7 @@ def check_answer_get_class_stats():
             5. Gladiator
             6. Priest
             >>> """))
-            condition = 0
+            end = True
 
         except ValueError:
             print("Your answer is not correct. Choose a number between 1 and 6")
@@ -166,33 +210,33 @@ def player_entity_maker(player_list, pseudonyme, password):
     adds a player with default settings to the player list
     """
 
-    condition = 1
-    while condition == 1:
+    end = False
+    while end == False:
         cls = check_answer_get_class_stats()
         if cls == 'Warrior':
             player_entity = Warrior()
-            condition = 0
+            end = True
         elif cls == 'Mage':
             player_entity = Mage()
-            condition = 0
+            end = True
         elif cls == 'Soldier':
             player_entity = Soldier()
-            condition = 0
+            end = True
         elif cls == 'Sorcerer':
             player_entity = Sorcerer()
-            condition = 0
+            end = True
         elif cls == 'Gladiator':
             player_entity = Gladiator()
-            condition = 0
+            end = True
         elif cls == 'Priest':
             player_entity = Priest()
-            condition = 0
+            end = True
         else:
             continue
 
-    player_list[pseudonyme] = (password, player_entity)
+    player_list[pseudonyme] = [password, player_entity]
 
-    return player_list, pseudonyme, player_entity
+    return player_list, pseudonyme, password, player_entity
 
 
 def check_login(player_list, pseudonyme, password):
@@ -229,11 +273,6 @@ def next_chapter():
     """
     changer les stats des mobs
     """
-
-def player_attack():
-    pass
-    if "VIGOR" in ...:
-        pass
 
 
 def get_check_pseudonyme(pseudonyme):
@@ -272,7 +311,8 @@ def login(player_list):
                 password = input("""What is your password ?:
                         >>> """)
 
-                return player_entity_maker(player_list, pseudonyme, password)
+                player_list, pseudonyme, password, player_entity = player_entity_maker(player_list, pseudonyme, password)
+                return player_list, pseudonyme, password, player_entity
 
             else:
                 login(player_list)
@@ -285,7 +325,8 @@ def login(player_list):
                 password = input("""What is your password ?:
                         >>> """)
 
-                return player_entity_maker(player_list, pseudonyme, password)
+                player_list, pseudonyme, password, player_entity = player_entity_maker(player_list, pseudonyme, password)
+                return player_list, pseudonyme, password, player_entity
 
             else:
                 login(player_list)
@@ -293,21 +334,32 @@ def login(player_list):
             password = input("""What is your password ?:
                     >>> """)
 
-            return player_entity_maker(player_list, pseudonyme, password)
+            player_list, pseudonyme, password, player_entity = player_entity_maker(player_list, pseudonyme, password)
+            return player_list, pseudonyme, password, player_entity
 
     else:
-        password = input("""What is your password ?:
-                >>> """)
+        end = False
+        while end == False:
+            password = input("""What is your password ?:
+                    >>> """)
 
-        if check_login(player_list, pseudonyme, password) == True:
-            player_entity = player_list[pseudonyme][1]
-            return player_list, pseudonyme, player_entity
+            if check_login(player_list, pseudonyme, password) == True:
+                end = True
+
+            else:
+                print("Wrong password")
+                continue
+
+        player_entity = player_list[pseudonyme][1]
+        password = player_list[pseudonyme][0]
+        return player_list, pseudonyme, password, player_entity
 
 
 def main(player_list):
     start_screen()
-    player_list, pseudonyme, player_entity = login(player_list)
+    player_list, pseudonyme, password, player_entity = login(player_list)
     welcome(pseudonyme, player_entity.get_player_stats()['CHAPTER'])
+    player_entity.player_level_up(player_list, pseudonyme)
 
 player_list = charge_file()
 main(player_list)
